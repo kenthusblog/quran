8c070eff1d725eabef1899a10b40c907248a8862
citogok <47094667+kenthusblog@users.noreply.github.com>
2021-11-17 01:23:29 +0700

    Update index.js

--- alquran/surah/index.js
+++ alquran/surah/index.js
@@ -15,7 +15,7 @@ window.onload = function () {
          let fragmentDaftarAyat = '';
 
          $('.nama-surat').text(namaSurat);
-         $('title').text(`Islamic Bit - Surah ${namaSurat}`)
+         $('title').text(`Moco Qur'an - Surah ${namaSurat}`)
 
          dataAyat.forEach(ayat => {
             const audio = ayat.audio.primary
