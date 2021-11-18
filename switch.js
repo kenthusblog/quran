b9f294cb29078deddbe14dff32317ab658ca25b4
citogok <47094667+kenthusblog@users.noreply.github.com>
2021-11-19 01:43:06 +0700

    Update switch.js

--- switch.js
+++ switch.js
@@ -1,99 +1,26 @@
-/**
- *  Light Switch @version v0.1.3
- */
+window.console = window.console || function(t) {};
+if (document.location.search.match(/type=embed/gi)) {
+  window.parent.postMessage("resize", "*");
+}
+console.log('Ka.');
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
 
-(function () {
-  let lightSwitch = document.getElementById("lightSwitch");
-  if (!lightSwitch) {
-    return;
-  }
-  
-  /**
-   * @function darkmode
-   * @summary: changes the theme to 'dark mode' and save settings to local stroage.
-   * Basically, replaces/toggles every CSS class that has '-light' class with '-dark'
-   */
-  function darkMode() {
-    document.querySelectorAll(".bg-light").forEach((element) => {
-      element.className = element.className.replace(/-light/g, "-dark");
-    });
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark'); //add this
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+    }    
+}
+toggleSwitch.addEventListener('change', switchTheme, false);
 
-    document.body.classList.add("bg-dark");
+const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
 
-    if (document.body.classList.contains("text-dark")) {
-      document.body.classList.replace("text-dark", "text-light");
-    } else {
-      document.body.classList.add("text-light");
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
     }
-    
-    // set light switch input to true
-    if (! lightSwitch.checked) {
-      lightSwitch.checked = true;
-    }
-    localStorage.setItem("lightSwitch", "dark");
-  }
-
-  /**
-   * @function lightmode
-   * @summary: changes the theme to 'light mode' and save settings to local stroage.
-   */
-  function lightMode() {
-    document.querySelectorAll(".bg-dark").forEach((element) => {
-      element.className = element.className.replace(/-dark/g, "-light");
-    });
-
-    document.body.classList.add("bg-light");
-
-    if (document.body.classList.contains("text-light")) {
-      document.body.classList.replace("text-light", "text-dark");
-    } else {
-      document.body.classList.add("text-dark");
-    }
-    
-    if (lightSwitch.checked) {
-      lightSwitch.checked = false;
-    }
-    localStorage.setItem("lightSwitch", "light");
-  }
-  
-  /**
-   * @function onToggleMode
-   * @summary: the event handler attached to the switch. calling @darkMode or @lightMode depending on the checked state.
-   */
-  function onToggleMode() {
-    if (lightSwitch.checked) {
-      darkMode();
-    } else {
-      lightMode();
-    }
-  }
-  
-  /**
-   * @function getSystemDefaultTheme
-   * @summary: get system default theme by media query
-   */
-  function getSystemDefaultTheme() {
-    const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
-    if (darkThemeMq.matches) {
-      return "dark";
-    }
-    return "light";
-  }
-
-  function setup() {
-    var settings = localStorage.getItem("lightSwitch");
-    if (settings == null) {
-      settings = getSystemDefaultTheme();
-    }
-    
-    if (settings == "dark") {
-      lightSwitch.checked = true;
-    }
-    
-    lightSwitch.addEventListener("change", onToggleMode);
-    onToggleMode();
-  }
-  
-  setup();
-  
-})();
+}
