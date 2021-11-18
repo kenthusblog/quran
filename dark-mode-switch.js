c3e89eb46ebb5e8a3894ccd5db64d0bf4efb7a8e
citogok <47094667+kenthusblog@users.noreply.github.com>
2021-11-19 01:14:15 +0700

    Update dark-mode-switch.js

--- dark-mode-switch.js
+++ dark-mode-switch.js
@@ -50,3 +50,61 @@ function resetTheme() {
     localStorage.removeItem("darkSwitch");
   }
 }
+
+
+
+$(document).ready(function () {
+  $('#theme_toggle').on('click', function () {
+    if ($('body').hasClass('dark-theme')) {
+      $(this).toggleClass('btn-light');
+      $(this).addClass('btn-dark');
+      $('h1').html("Light Mode");
+      $('body').toggleClass('dark-theme');
+      $('.card').toggleClass('dark-theme');
+      localStorage.setItem("mode", "light-theme");
+    } else {
+      $(this).toggleClass('btn-light');
+      $(this).removeClass('btn-dark');
+      $('h1').html("Dark Mode");
+      $('body').toggleClass('dark-theme');
+      $('.card').toggleClass('dark-theme');
+      localStorage.setItem("mode", "dark-theme");
+    }
+  })
+  //check for localStorage, add as browser preference if missing
+  if (!localStorage.getItem("mode")) {
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      localStorage.setItem("mode", "dark-theme");
+    } else {
+      localStorage.setItem("mode", "light-theme");
+    }
+  }
+  //set interface to match localStorage
+  if (localStorage.getItem("mode") == "dark-theme") {
+    $('#theme_toggle').removeClass('btn-dark');
+    $('#theme_toggle').addClass('btn-light');
+    $('body').addClass('dark-theme');
+    $('.card').addClass('dark-theme');
+    $('h1').html("Dark Mode");
+    document.getElementById("theme_toggle").checked = true;
+  } else {
+    $('#theme_toggle').addClass('btn-dark');
+    $('#theme_toggle').removeClass('btn-light');
+    $('body').removeClass('dark-theme');
+    $('.card').removeClass('dark-theme');
+    $('h1').html("Light Mode");
+    document.getElementById("theme_toggle").checked = false;
+  };
+});
+
+
+
+
+
+
+
+
+
+
+
+
