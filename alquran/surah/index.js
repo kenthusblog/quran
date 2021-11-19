af3e6a4e2ac8cff7c04679f1e5c0b25b63a550c8
citogok <47094667+kenthusblog@users.noreply.github.com>
2021-11-19 12:58:21 +0700

    Update index.js

--- alquran/surah/index.js
+++ alquran/surah/index.js
@@ -29,7 +29,7 @@ window.onload = function () {
                <div class="ayat p-3 p-sm-4">
                   <div class="mb-0 py-2 me-3">
                      <h3 class="nomer-ayat text-center" style="font-style: italic;">${nomer}.</h3>
-                     <span class="last-read far fa-bookmark" style="font-size: 1.8rem; cursor: pointer; onclick=myFunction();"></span>
+                     <span class="last-read far fa-bookmark" style="font-size: 1.8rem; cursor: pointer;"></span>
                </div>
                   <div class="detail-ayat mb-2 w-100">
                      <div class="bacaan-ayat mb-3">
@@ -238,7 +238,3 @@ function setToLocalStorage(value) {
 
 
 
-
-document.querySelector(".last-read").addEventListener('click', function(){
-  Swal.fire("Our First Alert");
-});
