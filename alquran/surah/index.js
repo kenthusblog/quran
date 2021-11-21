8d35ca7d919dc69012d4d7ea52a728ab0702f682
citogok <47094667+kenthusblog@users.noreply.github.com>
2021-11-21 15:10:18 +0700

    Update index.js

--- alquran/surah/index.js
+++ alquran/surah/index.js
@@ -29,9 +29,9 @@ window.onload = function () {
                <div class="ayat p-3 p-sm-4">
                   <div class="mb-0 py-2 me-3">
                      <h3 class="nomer-ayat text-center" style="font-style: italic;">${nomer}.</h3>
-                    <div class="popup" onclick="myFunction()">
+                    <div class="tooltip">
                     <span class="last-read far fa-bookmark" style="font-size: 1.8rem; cursor: pointer;"></span>
-                     <span class="popuptext" id="myPopup">Berhasil Disimpan</span></div>
+                     <span class="tooltiptext">Berhasil Disimpan</span></div>
                   </div>
                   <div class="detail-ayat mb-2 w-100">
                      <div class="bacaan-ayat mb-3">
