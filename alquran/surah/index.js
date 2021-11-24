570e09c6240de6cc0d68753407c5f58461927d28
citogok <47094667+kenthusblog@users.noreply.github.com>
2021-11-24 15:03:39 +0700

    Update index.js

--- alquran/surah/index.js
+++ alquran/surah/index.js
@@ -66,44 +66,50 @@ window.onload = function () {
 
          $('.daftar-ayat .container').html(fragmentDaftarAyat);
          
-         listayat.forEach(surat => {
-            nomer = surat.numberOfVerses
-            teksArab = surat.name.short
-            teksLatin = surat.revelation.id
-            tafsir = surat.tafsir.id
-
-            fragmentlistAyat += `
-               <div class="ayat p-3 p-sm-4">
-                  <div class="mb-0 py-2 me-3">
-                     <h3 class="nomer-ayat text-center" style="font-style: italic;">${nomer}.</h3>
-                     <span class="last-read far fa-bookmark" style="font-size: 1.8rem; cursor: pointer;"></span>
-                  </div>
-                  <div class="detail-ayat mb-2 w-100">
-                     <div class="bacaan-ayat mb-3">
-                              <h1 class="text-end m-0" style="font-weight: 600; line-height: 1.5;">${teksArab}</h1>
-                     </div>
-                     <div class="bacaan-latin" style="width: 100%; max-width: 700px;">
-                        <h5 style="font-style: italic;">${teksLatin}</h5>
-                     </div>
-                     <div class="info-ayat mb-3 mb-sm-0" style="overflow: hidden;">
-                        <div class="info mt-3" style="letter-spacing: 1px;">
-                           <div class="arti mb-3">
-                             
-                           </div>
-                           <div class="tafsir">
-                              <h6 class="m-0" style="font-weight: 600;">Tafsir:</h6>
-                              <h6 class="m-0" style="font-weight: 400;">${tafsir}</h6>
-                           </div>
+         const daftarSurat = results.data;
+      let fragmentDaftarSurat = '';
+      daftarSurat.forEach(surat => {
+         fragmentDaftarSurat += `
+            <div class="surat p-3 p-sm-4">
+               <h3 class="nomer-surat text-center mb-0 py-2 me-3" style="font-style: italic;">${surat.number}.</h3>
+               <div class="detail-surat mb-1">
+                  <a href="surah/?nama=${surat.name.transliteration.id}&nomer=${surat.number}"
+                  style="display: block; color: var(--blue2)" >
+                     <div class="nama-surat">
+                        <div div style="letter-spacing: .1rem;">
+                           <h4 class="nama mb-0" style="font-weight: 700;">${surat.name.transliteration.id}</h4>
+                           <h6 class="arti">${surat.name.translation.id}</h6>
                         </div>
+                        <h1 class="arab text-end mt-1 mt-sm-0 me-3">${surat.name.short}</h1>
+                     </div>
+                  </a>
+                  <div class="info-surat mb-3 mb-sm-0" style="overflow: hidden;">
+                     <div class="info mt-3" style="letter-spacing: 1px">
+                        <ul class="ayat m-0">
+                           <li>
+                              <h6 style="font-weight: 400;">Memiliki <span style="font-weight: 600;">${surat.numberOfVerses} ayat</span></h6>
+                           </li>
+                        </ul>
+                        <ul class="jenis m-0">
+                           <li>
+                              <h6 style="font-weight: 400;">Merupakan golongan surat <span style="font-weight: 600;">${surat.revelation.id}</span></h6>
+                           </li>
+                        </ul>
+                        <ul class="penjelasan m-0">
+                           <li style="line-height: 1.5rem">
+                              <h6 style="font-weight: 400;">${surat.tafsir.id}</h6>
+                           </li>
+                        </ul>
                      </div>
                   </div>
-
-                  
                </div>
-            `;
-         })
-         
-         $('.list-ayat .container').html(fragmentlistAyat);
+               <span class="expand-detail">
+                  <img src="../img/arrow-down.png">
+               </span>
+            </div>
+         `;
+
+         $('.list-ayat .container').html(fragmentDaftarSurat);
                 
 
          
