fe956455b9738fad271b0a3be29acb311416f640
citogok <47094667+kenthusblog@users.noreply.github.com>
2021-11-25 00:07:23 +0700

    Update index.js

--- alquran/surah/index.js
+++ alquran/surah/index.js
@@ -37,7 +37,7 @@ window.onload = function () {
                <div class="ayat p-3 p-sm-4">
                   <div class="mb-0 py-2 me-3">
                      <h3 class="nomer-ayat text-center" style="font-style: italic;">${nomer}.</h3>
-                     <span class="last-read far fa-bookmark" style="font-size: 1.8rem; cursor: pointer;"></span>
+                     <span class="last-read far fa-bookmark" style="font-size: 1.8rem; cursor: pointer;"><span class="tooltiptext">Berhasil Disimpan</span></span>
                   </div>
                   <div class="detail-ayat mb-2 w-100">
                      <div class="bacaan-ayat mb-3">
