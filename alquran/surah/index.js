84b86a003e3f29be96b20beff63afd600d81408e
citogok <47094667+kenthusblog@users.noreply.github.com>
2021-11-24 10:15:00 +0700

    Update index.js

--- alquran/surah/index.js
+++ alquran/surah/index.js
@@ -15,6 +15,7 @@ window.onload = function () {
          let fragmentDaftarAyat = '';
 
          $('.nama-surat').text(namaSurat);
+         $('.list').text(dataAyat);
          $('title').text(`Moco Quran - Surah ${namaSurat}`)
          
          dataAyat.forEach(ayat => {
