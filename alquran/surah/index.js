05f71a3ce4e327e589360fca9ca079253c8cdedd
citogok <47094667+kenthusblog@users.noreply.github.com>
2021-11-26 10:12:19 +0700

    Update index.js

--- alquran/surah/index.js
+++ alquran/surah/index.js
@@ -254,3 +254,39 @@ function setToLocalStorage(value) {
 //       }
 //    })
 // }
+
+
+
+
+
+
+$(document).on('click', '.btn_download', function(){
+
+                let get = $(this).data('nomer')
+
+                $.ajax({
+                    url: 'https://api.quran.sutanlab.id/surah/${nomerSurat}' + get,
+                    type: 'GET',
+                })
+                .done(function(data) {
+                    
+                    let url = ayat.audio.primary
+
+                    var filename = url.substring(url.lastIndexOf("/") + 1).split("?")[0];
+                    var xhr = new XMLHttpRequest();
+                    xhr.responseType = 'blob';
+                    xhr.onload = function() {
+                        var a = document.createElement('a');
+                        a.href = window.URL.createObjectURL(xhr.response);
+                        a.download = filename; 
+                        a.style.display = 'none';
+                        document.body.appendChild(a);
+                        a.click();
+                        delete a;
+                    };
+                    xhr.open('GET', url);
+                    xhr.send();
+
+                });
+
+            })
