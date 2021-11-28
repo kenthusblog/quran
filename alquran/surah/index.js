ab71c5dab95978eb9ccc928ae3a1f476bdd6c31e
citogok <47094667+kenthusblog@users.noreply.github.com>
2021-11-28 09:49:51 +0700

    Update index.js

--- alquran/surah/index.js
+++ alquran/surah/index.js
@@ -42,7 +42,17 @@ window.onload = function () {
                   
                   
                  <!-- Sharingbutton Facebook -->
-<a class="resp-sharing-button__link" href="https://facebook.com/sharer/sharer.php?u="surah/?nama=${nomer}" target="_blank" rel="noopener" aria-label="">
+<a class="resp-sharing-button__link">
+<a href="surah/?nama=${teksLatin}&nomer=${nomer}"
+                  style="display: block; color: var(--blue2)" >
+                     <div class="nama-surat">
+                        <div div style="letter-spacing: .1rem;">
+                           <h4 class="nama mb-0" style="font-weight: 700;">${surat.name.transliteration.id}</h4>
+                           <h6 class="arti">${surat.name.translation.id}</h6>
+                        </div>
+                        <h1 class="arab text-end mt-1 mt-sm-0 me-3">${surat.name.short}</h1>
+                     </div>
+                  </a>
   <div class="resp-sharing-button resp-sharing-button--facebook resp-sharing-button--small"><div aria-hidden="true" class="resp-sharing-button__icon resp-sharing-button__icon--solidcircle">
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 0C5.38 0 0 5.38 0 12s5.38 12 12 12 12-5.38 12-12S18.62 0 12 0zm3.6 11.5h-2.1v7h-3v-7h-2v-2h2V8.34c0-1.1.35-2.82 2.65-2.82h2.35v2.3h-1.4c-.25 0-.6.13-.6.66V9.5h2.34l-.24 2z"/></svg>
     </div>
