c41d34d5de3811644e644523cb23d9ae079f064a
citogok <47094667+kenthusblog@users.noreply.github.com>
2021-11-30 14:11:36 +0700

    Update tes.js

--- alquran/surah/tes.js
+++ alquran/surah/tes.js
@@ -3,9 +3,7 @@ function home_text(){
 }
 
 function information_text(){
-	alert('Sumber Data : \n'+
-			'Sumber data seperti ayat-ayat, terjemahan, tafsir dan murottal disediakan melalui API oleh SutanLab yang mana data-datanya dihimpun dari berbagai sumber seperti Kementerian Agama untuk terjemahan dan tafsir ayat dalam bahasa Indonesia, Quran Cloud untuk ayat-ayat, meta data ayat, dan audio murottal (Syekh. Mishary Rashid Al-Afasy murrotal edition), Al-Quran-ID-API untuk tafsir surat dalam bahasa Indonesia.
-			');
+	alert('Sumber data disediakan melalui API oleh SutanLab yang mana data-datanya dihimpun dari berbagai sumber seperti Kementerian Agama untuk terjemahan dan tafsir ayat dalam bahasa Indonesia, Quran Cloud untuk ayat-ayat, meta data ayat, dan audio murottal (Syekh. Mishary Rashid Al-Afasy murrotal edition), Al-Quran-ID-API untuk tafsir surat dalam bahasa Indonesia.');
 }
 
 function portofolio_text(){
