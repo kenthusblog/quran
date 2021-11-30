cfa41df3d2ad0f4d5da97fab3d6052099e97ee82
citogok <47094667+kenthusblog@users.noreply.github.com>
2021-11-30 12:33:37 +0700

    Update index.js

--- alquran/surah/index.js
+++ alquran/surah/index.js
@@ -40,7 +40,7 @@ window.onload = function () {
                <div class="ayat p-3 p-sm-4">
               <div class="mb-0 py-2 me-3">                 
               <h3 class="nomer-ayat text-center" style="font-style: italic;">${nomer}.</h3>
-                     <span class="last-read far fa-bookmark" style="font-size: 1.8rem; cursor: pointer;"></span>
+                     <span class="last-read far fa-bookmark" style="font-size: 1.8rem; cursor: pointer;" onclick="home_text();"></span>
                   </div>
                   <div class="detail-ayat mb-2 w-100">
                      <h1 class="text-end m-0" style="font-weight: 600; line-height: 1.5;">${teksArab}</h1>
@@ -128,7 +128,7 @@ window.onload = function () {
                   this.classList.remove('fas')
                   this.classList.add('far')
 
-                  const islamicBit = JSON.parse(localStorage.getItem('ISLAMIC BIT'))
+                  const islamicBit = JSON.parse(localStorage.getItem('Moco Quran'))
                   delete islamicBit.bacaanTerakhir
 
                   setToLocalStorage(islamicBit)
@@ -243,3 +243,8 @@ function setToLocalStorage(value) {
 //       }
 //    })
 // }
+
+
+function home_text(){
+	alert('berhasil disimpan');
+}
